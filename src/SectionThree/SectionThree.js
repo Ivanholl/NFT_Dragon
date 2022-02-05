fd03defb5b82e864b41310970c0f1b4d2ba085f6
Ivan Jelev <ivan.jelev@abv.bg>
2022-02-05 12:52:20 +0200

    better mobile roadmap

--- src/SectionThree/SectionThree.js
+++ src/SectionThree/SectionThree.js
@@ -9,6 +9,7 @@ import { sections, roadmap } from '../texts'
 import './SectionThree.scss'
 
 const Goal = ({isMain, title, text}) => {
+
   return (<div className="goal-container flex" >    
     {isMain ? <img src="../img/MainGoalConnector.svg" className="main-goal-connector"/> : <img src="../img/GoalConnector.svg" className="goal-connector"/>}
     <div className="goal" >
@@ -33,31 +34,54 @@ const empty = () => <div className="goal-container empty flex">{'   '}</div>
 
 const SectionThree = () => {
   const { phase1, phase2, phase3 } = roadmap
+  const winWidth = window.screen.availWidth
+  const isSmall = winWidth > 400
   
   // phase1 goalMain goal1 title text
 
   return (<div id={sections[2].replaceAll(' ', '')} className="section-three">
     <SectionTitle titleText={sections[2]} />
     <div className="roadmap flex row center">
-      <div className="left flex col">
-        <Goal isMain title={phase1.goalMain}/>
-        <Goal title={phase1.goal2.title} text={phase1.goal2.text} />
-        {empty()}
-        <Goal title={phase2.goal1.title} text={phase2.goal1.text}/>
-        <Goal title={phase1.goal2.title} text={phase2.goal2.text} />
-        <Goal isMain title={phase3.goalMain}/>
-        <Goal title={phase3.goal2.title} text={phase3.goal2.text} />
-      </div>
-      <div className="border" />
-      <div className="right flex col">
-        <Goal title={phase1.goal1.title} text={phase1.goal1.text}/>
-        <Goal title={phase1.goal3.title} text={phase1.goal3.text}/>
-        <Goal isMain title={phase2.goalMain}/>
-        <Goal title={phase2.goal3.title} text={phase2.goal3.text}/>
-        {empty()}
-        <Goal title={phase3.goal1.title} text={phase3.goal1.text}/>
-        <Goal title={phase3.goal3.title} text={phase3.goal3.text}/>
-      </div>
+      {isSmall 
+        ? <>
+          <div className="left flex col">
+            <Goal isMain title={phase1.goalMain}/>
+            <Goal title={phase1.goal2.title} text={phase1.goal2.text} />
+            {empty()}
+            <Goal title={phase2.goal1.title} text={phase2.goal1.text}/>
+            <Goal title={phase1.goal2.title} text={phase2.goal2.text} />
+            <Goal isMain title={phase3.goalMain}/>
+            <Goal title={phase3.goal2.title} text={phase3.goal2.text} />
+          </div>
+          <div className="border" />
+          <div className="right flex col">
+            <Goal title={phase1.goal1.title} text={phase1.goal1.text}/>
+            <Goal title={phase1.goal3.title} text={phase1.goal3.text}/>
+            <Goal isMain title={phase2.goalMain}/>
+            <Goal title={phase2.goal3.title} text={phase2.goal3.text}/>
+            {empty()}
+            <Goal title={phase3.goal1.title} text={phase3.goal1.text}/>
+            <Goal title={phase3.goal3.title} text={phase3.goal3.text}/>
+          </div>
+        </>
+        : <>
+          <div className="left flex col">
+            <Goal isMain title={phase1.goalMain}/>
+            <Goal title={phase2.goal1.title} text={phase2.goal1.text}/>
+            <Goal title={phase1.goal2.title} text={phase1.goal2.text}/>
+            <Goal title={phase1.goal3.title} text={phase1.goal3.text}/>
+            <Goal isMain title={phase2.goalMain}/>
+            <Goal title={phase2.goal1.title} text={phase2.goal1.text}/>
+            <Goal title={phase2.goal2.title} text={phase2.goal2.text}/>
+            <Goal title={phase2.goal3.title} text={phase2.goal3.text}/>
+            <Goal isMain title={phase3.goalMain}/>
+            <Goal title={phase3.goal1.title} text={phase3.goal1.text}/>
+            <Goal title={phase3.goal2.title} text={phase3.goal2.text}/>
+            <Goal title={phase3.goal3.title} text={phase3.goal3.text}/>
+          </div>
+          <div className="border" />
+        </>
+      }
     </div>
   </div>)
 }
